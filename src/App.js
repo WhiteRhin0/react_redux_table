c7c3e1861fe8e39ecbcde488b36a2fab7970a87e
Oleksii Spalylo <white.rh42@gmail.com>
2021-02-18 20:47:33 +0200

    create table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
+import React, { useState } from 'react';
 import './App.css';
 
 import { Table } from './components/Table';
 
 function App() {
+  const [row, setRow] = useState(0);
+  const [column, setColumn] = useState(0);
+
   return (
     <div className="App">
-     <Table />
+      <form>
+
+        <label>
+          Columns:
+          <input
+            type="number"
+            name="column" 
+            value={column}
+            onChange={e => setColumn(e.target.value)}
+          />
+        </label>
+
+        <label>
+          Rows:
+          <input
+            type="number"
+            name="row"
+            value={row}
+            onChange={e => setRow(e.target.value)}
+          />
+        </label>
+
+      </form>
+      <Table 
+        row={row}
+        column={column}
+      />
     </div>
   );
 }
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import './Table.css';
 import { createArrayOfRandom } from '../../createArrayofRandom';
 
-export const Table = () => {
+export const Table = ({ row, column }) => {
+  let arrayOfrandom = createArrayOfRandom(row, column);
 
   return (
     <div>
-      <h1>Table</h1>
-      <div>{createArrayOfRandom(5, 10)}</div>
+      <table>
+        <thead>
+          <tr>
+            <th colspan={row}>The table header</th>
+          </tr>
+        </thead>
+        <tbody>
+          {arrayOfrandom.map(col => (
+            <tr>
+              {col.map(row => (
+                <td>{row}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
